721597abb818e5d7a0a7964f3aa26116d8678bd6
ismael20241994 <deazevedoismael0@gmail.com>
2026-08-11 22:49:16 +0200

    teste db

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -15,17 +15,18 @@ const pool = await mysql.createPool({
 pool.getConnection()
   .then(connection => {
     console.log('✅ MYSQL CONECTADO');
-    console.log({
+  
+    connection.release();
+  })
+  .catch(error => {
+    console.log('❌ ERRO MYSQL:', error.message);
+      console.log({
   DB_HOST: process.env.DB_HOST,
   DB_PORT: process.env.DB_PORT,
   DB_USER: process.env.DB_USER,
   DB_NAME: process.env.DB_NAME,
   DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD
   });
-    connection.release();
-  })
-  .catch(error => {
-    console.log('❌ ERRO MYSQL:', error.message);
   });
 
 
