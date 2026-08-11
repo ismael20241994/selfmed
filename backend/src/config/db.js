a6e5d9320d03e80ab13a80bb6a262d4bc29f0e4e
ismael20241994 <deazevedoismael0@gmail.com>
2026-08-11 22:46:41 +0200

    testando chang db config

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -15,6 +15,13 @@ const pool = await mysql.createPool({
 pool.getConnection()
   .then(connection => {
     console.log('✅ MYSQL CONECTADO');
+    console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD_EXISTS: !!process.env.DB_PASSWORD
+  });
     connection.release();
   })
   .catch(error => {
